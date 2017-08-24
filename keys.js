710cdc27b05acee6ce7cc6405c4ab066588c967f
chunchuprati <prathibha.panga@gmail.com>
2017-08-23 21:32:51 -0700

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,13 +1,2 @@
 console.log('this is loaded');
 
-exports.twitterKeys = {
-  consumer_key: 'BsUq2nxFT7yuaXTwf4Wwgh9Rv',
-  consumer_secret: '6A1iDZuotLGlhjWyJxj5onnTZ77fK1OpOnwRFR2j8JruGxbXip',
-  access_token_key: '898662938025771008-gfYPKvEz1JdfVxfdmKUgCWIVwhvP0lS',
-  access_token_secret: 'b9MyzuT7TNDMec4SxezdVhSYID8cqopMXvStYVhiGieGu'
-}
-
-exports.spotifyKeys = {
-  id: '24db97907b1d46e19435545934c9aa85',
-  secret: '1eb58c9affe74270ad87388879156d8d'
-}
